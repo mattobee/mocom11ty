569d7efdf8d07558a15ee0195bd5fabe1175b3db
Matt Obee <github@mattobee.com>
2020-07-20 19:38:42 +0100

    Highlighting menu items when scrolling. Buggy.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,26 @@
-import list from './modules/list'
+window.addEventListener('DOMContentLoaded', () => {
 
-console.log(`Hello ${list[0]}`)
+    const sections = document.querySelectorAll('.section[id]');
+
+    const config = {
+        threshold: [0, 0.25, 0.75, 1]
+      };      
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            const id = entry.target.getAttribute('id');
+            console.log(entry);
+            if (entry.intersectionRatio > 0.5) {
+                console.log(entry.intersectionRatio);
+                console.log('in the view', id);
+                document.querySelector(`nav li a[href="#${id}"]`).classList.add('current');
+            } else {
+                console.log('out of view', id);
+                document.querySelector(`nav li a[href="#${id}"]`).classList.remove('current');
+            }
+        }, config);
+    }, config);
+
+    sections.forEach(section => { observer.observe(section); });
+	
+});
