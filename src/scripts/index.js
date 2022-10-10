2501f0182673e8aab77b70895816bc30fb7adf63
Matt Obee <github@mattobee.com>
2022-10-10 18:10:07 +0100

    Make the bio copy button work

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,26 +1,12 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-    const sections = document.querySelectorAll('.section[id]');
+    const btn_copy = document.getElementById('copy')
+    const bio = document.getElementById('bio')
 
-    const config = {
-        threshold: [0, 0.25, 0.75, 1]
-      };      
-
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            const id = entry.target.getAttribute('id');
-            console.log(entry);
-            if (entry.intersectionRatio > 0.5) {
-                console.log(entry.intersectionRatio);
-                console.log('in the view', id);
-                document.querySelector(`nav li a[href="#${id}"]`).classList.add('current');
-            } else {
-                console.log('out of view', id);
-                document.querySelector(`nav li a[href="#${id}"]`).classList.remove('current');
-            }
-        }, config);
-    }, config);
-
-    sections.forEach(section => { observer.observe(section); });
+    copy.onclick = () => {
+        const value = bio.textContent
+        navigator.clipboard.writeText(value)
+        console.log("clicking copy")
+    }
 	
 });
