946277d41d3abcc4bcb4508308fba01f723c60d5
Matt Obee <matt@obee.me>
2023-09-22 22:24:24 +0100

    Fix scripts not being passed through to build

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,16 @@
 const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
 
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('src/images');
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("scripts");
+
   eleventyConfig.addPlugin(eleventySass);
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
   return {
-    dir: { input: 'src', output: 'dist', data: '_data' },
+    dir: { input: "src", output: "dist", data: "_data" },
     passthroughFileCopy: true,
-    templateFormats: ['njk', 'md', 'css', 'html', 'yml', 'png'],
-    htmlTemplateEngine: 'njk'
-  }
-}
+    templateFormats: ["njk", "md", "css", "html", "yml", "png"],
+    htmlTemplateEngine: "njk",
+  };
+};
