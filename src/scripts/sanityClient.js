514846fe51b88e878d75142e968900d0b05661ff
Matt Obee <matt@obee.me>
2024-10-03 10:51:42 +0100

    Pull Sanity API token from Netlify envars

--- src/scripts/sanityClient.js
+++ src/scripts/sanityClient.js
@@ -1,10 +1,12 @@
 import { createClient } from '@sanity/client';
+const apiToken = process.env.SANITY;
 
 const client = createClient({
   projectId: 'eb20sjiu',
   dataset: 'production',
   apiVersion: '2024-10-03',
   useCdn: true,
+  token: apiToken
 });
 
 export default client;
